68bcd62f8a8b70197f2b9999a8ad36390173a1e8
Sandra <sandra.albin@hotmail.com>
2022-04-26 00:48:40 -0300

    Subo test de obtener ventas por id

--- src/servidor.js
+++ src/servidor.js
@@ -1,46 +1,70 @@
-import express from 'express'
-import { obtenerVentaPorId, obtenerVentas, insertarVenta } from './ventas.js'
+import express from "express";
+import { obtenerVentaPorId, obtenerVentas, insertarVenta } from "./ventas.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/ventas', (req, res) => {
-    const ventas = obtenerVentas()
-    res.json(ventas)
-})
+//cual forma sería mejor para enviar parametros
+// GET /ventas?id=1 & y otro paramentro --> de esta manera se pueden enviar varios parametros
+// se usa el query
+// y valida ventastotales y ventasporid
+app.get("/ventas", (req, res) => {
+  const id = req.query.id;
+  let ventas = null;
+  if (id) {
+    ventas = obtenerVentaPorId(id);
+  } else {
+    ventas = obtenerVentas();
+  }
+  res.json(ventas);
+});
 
-app.post('/ventas', (req, res) => {
-    try {
-        const venta = req.body
-        const ventaAgregada = insertarVenta(venta)
-        res.status(201).json(ventaAgregada)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+// GET /ventas/1
+//se usa el param
+//se hacen 2 get, 1 para cada metodo
+app.get("/ventas/:id", (req, res) => {
+  const id = req.params.id;
+  const venta = obtenerVentaPorId(id);
+  res.json(venta);
+});
 
-let server
+app.get("/ventas", (req, res) => {
+    const ventas = obtenerVentas();
+    res.json(ventas);
+  });
+
+app.post("/ventas", (req, res) => {
+  try {
+    const venta = req.body;
+    const ventaAgregada = insertarVenta(venta);
+    res.status(201).json(ventaAgregada);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+let server;
 
 export function conectar() {
-    return new Promise((resolve, reject) => {
-        server = app.listen(3000, () => {
-            resolve(server.address().port)
-        })
-        server.on('error', error => {
-            reject(error)
-        })
-    })
+  return new Promise((resolve, reject) => {
+    server = app.listen(3000, () => {
+      resolve(server.address().port);
+    });
+    server.on("error", (error) => {
+      reject(error);
+    });
+  });
 }
 
 export function desconectar() {
-    return new Promise((resolve, reject) => {
-        server.close(error => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve()
-            }
-        })
-    })
+  return new Promise((resolve, reject) => {
+    server.close((error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
 }
--- test/servidor.test.js
+++ test/servidor.test.js
@@ -1,70 +1,85 @@
-import assert from 'assert'
-import axios from 'axios'
-import { conectar, desconectar } from '../src/servidor.js'
+import assert from "assert";
+import axios from "axios";
+import { conectar, desconectar } from "../src/servidor.js";
 
-import { obtenerVentas } from '../src/ventas.js'
+import { obtenerVentas } from "../src/ventas.js";
+import { obtenerVentaPorId } from "../src/ventas.js"
 
-describe('servidor de pruebas', () => {
+describe("servidor de pruebas", () => {
+  before(async () => {
+    await conectar();
+  });
 
-    before(async () => {
-        await conectar()
-    })
+  after(async () => {
+    await desconectar();
+  });
 
-    after(async () => {
-        await desconectar()
-    })
+  describe("el servidor esta escuchando", () => {
+    describe("al pedirle las ventas", () => {
+      it("devuelve un array con ventas", async () => {
+        const { data: ventasObtenidas, status } = await axios.get(
+          "http://localhost:3000/ventas"
+        );
+        assert.strictEqual(status, 200);
+        const ventasReales = obtenerVentas();
+        assert.deepStrictEqual(ventasObtenidas, ventasReales);
+      });
+    });
 
-    describe('el servidor esta escuchando', () => {
+    describe("al pedirle una ventas por id", () => {
+        it("devuelve una venta", async () => {
+          const { data: ventaObtenida, status } = await axios.get(
+            "http://localhost:3000/ventas/1"
+          );
+          assert.strictEqual(status, 200);
+          const ventaReal = obtenerVentaPorId('1');
+          assert.deepStrictEqual(ventaObtenida, ventaReal);
+        });
+      });
 
-            describe('al pedirle las ventas', () => {
-                it('devuelve un array con ventas', async () => {
-                    const { data: ventasObtenidas, status } = await axios.get('http://localhost:3000/ventas')
-                    assert.strictEqual(status, 200)
-                    const ventasReales = obtenerVentas()
-                    assert.deepStrictEqual(ventasObtenidas, ventasReales)
-                })
-            })
-        
-        describe('al mandarle una venta', () => {
-            it('la agrega a las demas existentes', async () => {
-                const ventasAntes = obtenerVentas()
-                const venta = {
-                    idUsuario: 1,
-                    productos: [
-                        {
-                            idProducto: 1,
-                            nombreProducto : 'Heladera',
-                            precioProducto : 60000
-                        }
-                    ]
-                }
-                const { data: ventaAgregada, status } = await axios.post('http://localhost:3000/ventas', venta)
-                assert.strictEqual(status, 201)
+    describe("al mandarle una venta", () => {
+      it("la agrega a las demas existentes", async () => {
+        const ventasAntes = obtenerVentas();
+        const venta = {
+          idUsuario: 1,
+          productos: [
+            {
+              idProducto: 1,
+              nombreProducto: "Heladera",
+              precioProducto: 60000,
+            },
+          ],
+        };
+        const { data: ventaAgregada, status } = await axios.post(
+          "http://localhost:3000/ventas",
+          venta
+        );
+        assert.strictEqual(status, 201);
 
-                const ventasDespues = obtenerVentas()
-                assert.strictEqual(ventasDespues.length, ventasAntes.length + 1)
-            })
-        })
+        const ventasDespues = obtenerVentas();
+        assert.strictEqual(ventasDespues.length, ventasAntes.length + 1);
+      });
+    });
 
-        describe('al mandarle una venta mal formateada', () => {
-            it('no agrega nada y devuelve un error', async () => {
-                const ventasAntes = obtenerVentas()
-                const venta = {
-                    id : 2,
-                    catalogo : 'Linea blanca'
-                }
+    describe("al mandarle una venta mal formateada", () => {
+      it("no agrega nada y devuelve un error", async () => {
+        const ventasAntes = obtenerVentas();
+        const venta = {
+          id: 2,
+          catalogo: "Linea blanca",
+        };
 
-                await assert.rejects(
-                    axios.post('http://localhost:3000/ventas', venta),
-                    error => {
-                        assert.strictEqual(error.response.status, 400)
-                        return true
-                    }
-                )
+        await assert.rejects(
+          axios.post("http://localhost:3000/ventas", venta),
+          (error) => {
+            assert.strictEqual(error.response.status, 400);
+            return true;
+          }
+        );
 
-                const ventasDespues = obtenerVentas()
-                assert.deepStrictEqual(ventasDespues, ventasAntes)
-            })
-        })
-    })
-})
+        const ventasDespues = obtenerVentas();
+        assert.deepStrictEqual(ventasDespues, ventasAntes);
+      });
+    });
+  });
+});
