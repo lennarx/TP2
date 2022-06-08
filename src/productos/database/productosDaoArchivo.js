89d5c75d3de70d64cb871effe18ebc3d6e43b3c6
rubenrafael1983 <rubenrafael1983@gmail.com>
2022-06-08 10:29:24 -0300

    Refactor Dao

---
+++ src/productos/database/productosDaoArchivo.js
@@ -0,0 +1,46 @@
+const productos = [];
+
+const copiarProducto = (producto) => {
+    return ({ id: producto.id, idUsuario: producto.idUsuario, productos: producto.productos })
+}
+
+const copiarProductos = (productos) => {
+    return productos.map(copiarProducto)
+}
+
+export function recuperarProductos() {
+    return copiarProductos(productos)
+}
+
+export function guardarProducto (producto) {
+    const indiceBuscado = productos.findIndex(p => p.id === producto.id)
+    if (indiceBuscado === -1) {
+        productos.push(producto)
+    } else {
+        productos[indiceBuscado] = producto
+    }
+}
+
+export function borrarProductos () {
+    while (productos.length > 0) {
+        productos.pop()
+    }
+}
+
+export function obtenerProductoPorId (idProducto) {
+    const productoBuscada = productos.find(p => p.id.toString() === idProducto)
+    if (productoBuscada) {
+        return copiarProducto(productoBuscada)
+    } else {
+        throw new Error('Producto no encontrado')
+    }
+}
+
+export function borrarProductoPorId (idProducto) {
+    const indiceBuscado = productos.findIndex(p => p.id === idProducto)
+    if (indiceBuscado === -1) {
+        throw new Error('Producto no encontrad0')
+    } else {
+        productos.splice(indiceBuscado, 1)
+    }
+}
---
+++ src/productos/database/productosDaoMemoria.js
@@ -0,0 +1,46 @@
+const productos = [];
+
+const copiarProducto = (producto) => {
+    return ({ id: producto.id, idUsuario: producto.idUsuario, productos: producto.productos })
+}
+
+const copiarProductos = (productos) => {
+    return productos.map(copiarProducto)
+}
+
+export const recuperarProductos = () => {
+    return copiarProductos(productos)
+}
+
+export const guardarProducto = (producto) => {
+    const indiceBuscado = productos.findIndex(p => p.id === producto.id)
+    if (indiceBuscado === -1) {
+        productos.push(producto)
+    } else {
+        productos[indiceBuscado] = producto
+    }
+}
+
+export const borrarProductos = () => {
+    while (productos.length > 0) {
+        productos.pop()
+    }
+}
+
+export const obtenerProductoPorId = idProducto => {
+    const productoBuscado = productos.find(v => p.id.toString() === idProducto)
+    if (productoBuscado) {
+        return copiarProducto(productoBuscado)
+    } else {
+        throw new Error('Producto no encontrado')
+    }
+}
+
+export const borrarProductoPorId = (idProducto) => {
+    const indiceBuscado = productos.findIndex(c => c.id === idProducto)
+    if (indiceBuscado === -1) {
+        throw new Error('Producto no encontrado')
+    } else {
+        productos.splice(indiceBuscado, 1)
+    }
+}
