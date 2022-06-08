c23204c1e48c294e8ab4482274c45f085873676e
Franco Redoni <lennarx@outlook.com>
2022-06-08 12:19:24 -0300

    Refactor router controller

--- src/productos/routerProductos.js
+++ src/productos/routerProductos.js
@@ -1,62 +1,22 @@
 import { Router } from 'express'
 import {
-    obtenerProductos,
-    obtenerProductoPorId,
-    insertarProducto,
-    borrarProductoSegunId,
-    reemplazarProducto,
-} from './productos.js'
+    deleteProductoController,
+    getProductoPorIdController,
+    getProductosController,
+    postProductoController,
+    putProductoController
+} from './ProductosController.js'
 
 const routerProductos = new Router()
 
-routerProductos.get('/', (req, res) => {
-    let productos
+routerProductos.get('/', getProductosController)
 
-    productos = obtenerProductos()
+routerProductos.get('/:id', getProductoPorIdController)
 
-    res.json(productos)
-})
+routerProductos.post('/', postProductoController)
 
-routerProductos.get('/:id', (req, res) => {
-    try {
-        const producto = obtenerProductoPorId(req.params.id)
-        res.json(producto)
-    } catch (error) {
-        res.status(404).json({ error: error.message })
-    }
-})
+routerProductos.delete('/:id', deleteProductoController)
 
-routerProductos.post('/', (req, res) => {
-    try {
-        const producto = req.body
-        const productoAgregado = insertarProducto(producto)
-        res.status(201).json(productoAgregado)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
-
-routerProductos.delete('/:id', (req, res) => {
-    try {
-        borrarProductoSegunId(req.params.id)
-        res.sendStatus(204)
-    } catch (error) {
-        res.status(404).json({ error: error.message })
-    }
-})
-
-routerProductos.put('/:id', (req, res) => {
-    try {
-        const datosActualizados = req.body
-        const productoActualizado = reemplazarProducto(req.params.id, datosActualizados)
-        res.json(productoActualizado)
-    } catch (error) {
-        if (error.tipo == 'not found') {
-            res.status(404).json({ error: error.message })
-        } else {
-            res.status(400).json({ error: error.message })
-        }
-    }
-})
+routerProductos.put('/:id', putProductoController)
 
 export { routerProductos }
