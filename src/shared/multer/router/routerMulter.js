f008c2cc448bd166aafc9ac5981a6cad0cd49357
Gaston Castro Martinez <Gastoncastromartinez@gmail.com>
2022-06-22 11:41:11 -0300

    nuevo mergeo

--- src/shared/multer/router/routerMulter.js
+++ src/shared/multer/router/routerMulter.js
@@ -1,6 +1,11 @@
 import {extraerUnArchivo} from '../middleware/middlewareArchivo.js'
 import {postFotosController} from '../controller/controllerMulter.js'
 
-const routerMulter = new Router();
+const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+const routerMulter = new Router (); 
 routerMulter.post('/fotos', extraerUnArchivo, postFotosController)
